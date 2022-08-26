9138d53c8b616c6b75b27e9335489d8287b9b1c4
Max Garcia <maxdanielsanchezgarcia@gmail.com>
2022-08-25 23:01:17 -0400

    Header links erasing

--- src/components/section-titles.js
+++ src/components/section-titles.js
@@ -3,7 +3,6 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container"
 import Styles from '../sass/section-titles.module.scss'
-import Stack from 'react-bootstrap/Stack'
 
 function MainTitle(){
     
@@ -11,28 +10,19 @@ function MainTitle(){
         <Container fluid id={Styles.mainTitle} className='p-0 mt-5 position-absolute'>
             <Row className='m-0'>
                 <Col xs={{offset:2}} className='p-0'>
-                    <p className='text-white m-0 ms-4'>Hey there, I'm</p>
+                    <p className='text-white m-0 ms-3'>Hey there, I'm</p>
                 </Col>
             </Row>
             <Container fluid className='p-0 bg-secondary'>
                 <Row className="d-flex align-items-center p-0 m-0">
-                    <Col xs='3' className="h-100 p-0">
+                    <Col xs='2' className="h-100 p-0">
                         <span className={`w-100 bg-primary d-block ${Styles.titleLine}`}></span>
                     </Col>
-                    <Col xs='5 p-0 d-flex'>
+                    <Col xs='auto p-0 d-flex px-2'>
                         <h1 className="text-primary mx-auto text-nowrap my-0 lh-1 pt-2">Max Garcia</h1>
                     </Col>
                     <Col className="h-100 p-0">
                         <span className={`w-100 bg-primary d-block ${Styles.titleLine}`}></span>
-                    </Col>
-                </Row>
-                <Row className='m-0 mt-n1'>
-                    <Col xs={{span:'5', offset:3}} className='d-flex pb-1'>
-                        <Stack direction="horizontal" gap={6} className="mx-auto">
-                            <a href="#about" class={Styles.headerLinks}>About</a>
-                            <a href="#projects" class={Styles.headerLinks}>Projects</a>
-                            <a href="#contact" class={Styles.headerLinks}>Contact</a>
-                        </Stack>
                     </Col>
                 </Row>
             </Container>
@@ -55,13 +45,13 @@ function ProjectsTitle(){
     
     return (
         <div id={Styles.projectsTitle} className="d-flex position-absolute p-0">
-            <img fluid src="./title-ends/title-end-left.png"/>
+            <img src="./title-ends/title-end-left.png" alt=''/>
             <div className="d-flex bg-secondary">
                 <span className={`my-auto w-100 bg-primary ${Styles.titleLine}`}></span>
                 <h2 className="text-primary mx-3 text-nowrap my-0">Projects</h2>
                 <span className={`my-auto w-100 bg-primary ${Styles.titleLine}`}></span>
             </div>
-            <img fluid src="./title-ends/title-end-right.png"/>
+            <img src="./title-ends/title-end-right.png" alt=''/>
         </div>
     )
 }
